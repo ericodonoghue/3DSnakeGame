9df26022dee761812fbe785a5a196ce68d4252b2
Eric O'Donoghue <ericodonoghue27@gmail.com>
2021-10-11 11:20:39 -0600

    visual studio solution setup and main.cpp creation

---
+++ 3D_snake_game/3D_snake_game/main.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+#include <windows.h>
+#include <GL/glut.h>
+
+// movement detection
+#define UP 0
+#define Down 1
+#define LEFT 2
+#define RIGHT 3
+
+// game variables
+GLint points = 0;
+GLbyte game_state = false;
+
+int main(int argc, char** argv)
+{
+	return 0;
+}
